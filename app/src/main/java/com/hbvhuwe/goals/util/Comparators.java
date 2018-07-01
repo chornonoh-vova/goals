5fed2da0d0b96395bb1b9ee2a91a36e2c5d69176
hbvhuwe <chernonog.vova@gmail.com>
2018-07-01 19:48:39 +0300

    Generify Comparators

--- app/src/main/java/com/hbvhuwe/goals/util/Comparators.java
+++ app/src/main/java/com/hbvhuwe/goals/util/Comparators.java
@@ -7,75 +7,53 @@ import com.hbvhuwe.goals.model.Stage;
 import java.util.Comparator;
 
 public final class Comparators {
-    private static final Comparator BY_DEFAULT = new Comparator<Model>() {
-        @Override
-        public int compare(Model o1, Model o2) {
-            return o2.getId() - o1.getId();
-        }
-    };
+    public static final Comparator<Goal> BY_GOAL_TITLE_ASC = compareBy(true, true);
+    public static final Comparator<Goal> BY_GOAL_TITLE_DESC = compareBy(true, false);
+    public static final Comparator<Goal> BY_GOAL_ID_ASC = compareBy(false, true);
+    public static final Comparator<Goal> BY_GOAL_ID_DESC = compareBy(false, false);
 
-    public static final Comparator<Goal> BY_GOAL_DEFAULT = BY_DEFAULT;
-    public static final Comparator<Goal> BY_GOAL_TITLE_ACS = compareGoalBy(true, true);
-    public static final Comparator<Goal> BY_GOAL_TITLE_DESC = compareGoalBy(true, false);
-    public static final Comparator<Goal> BY_GOAL_ID_ASC = compareGoalBy(false, true);
-    public static final Comparator<Goal> BY_GOAL_ID_DESC = BY_GOAL_DEFAULT;
+    public static final Comparator<Stage> BY_STAGE_TITLE_ASC = compareBy(true, true);
+    public static final Comparator<Stage> BY_STAGE_TITLE_DESC = compareBy(true, false);
+    public static final Comparator<Stage> BY_STAGE_ID_ASC = compareBy(false, true);
+    public static final Comparator<Stage> BY_STAGE_ID_DESC = compareBy(false, false);
 
-    public static final Comparator<Stage> BY_STAGE_DEFAULT = BY_DEFAULT;
-    public static final Comparator<Stage> BY_STAGE_TITLE_ASC = compareStageBy(true, true);
-    public static final Comparator<Stage> BY_STAGE_TITLE_DESC = compareStageBy(true, false);
-    public static final Comparator<Stage> BY_STAGE_ID_ASC = compareStageBy(false, true);
-    public static final Comparator<Stage> BY_STAGE_ID_DESC = BY_STAGE_DEFAULT;
-
-    private static Comparator<Goal> compareGoalBy(boolean title, final boolean asc) {
-        Comparator<Goal> res = BY_GOAL_DEFAULT;
+    @SuppressWarnings("unchecked")
+    private static <T extends Model> Comparator<T> compareBy(boolean title, boolean asc) {
+        Comparator<Model> res;
         if (title) {
-            res = new Comparator<Goal>() {
-                @Override
-                public int compare(Goal o1, Goal o2) {
-                    if (asc) {
-                        return o1.getTitle().compareTo(o2.getTitle());
-                    } else {
-                        return o2.getTitle().compareTo(o1.getTitle());
+            if (asc) {
+                res = new Comparator<Model>() {
+                    @Override
+                    public int compare(Model o1, Model o2) {
+                        return o1.getTitle().compareToIgnoreCase(o2.getTitle());
                     }
-                }
-            };
+                };
+            } else {
+                res = new Comparator<Model>() {
+                    @Override
+                    public int compare(Model o1, Model o2) {
+                        return o2.getTitle().compareToIgnoreCase(o1.getTitle());
+                    }
+                };
+            }
         } else {
             if (asc) {
-                res = new Comparator<Goal>() {
+                res = new Comparator<Model>() {
                     @Override
-                    public int compare(Goal o1, Goal o2) {
+                    public int compare(Model o1, Model o2) {
                         return o1.getId() - o2.getId();
+                    }
+                };
+            } else {
+                res = new Comparator<Model>() {
+                    @Override
+                    public int compare(Model o1, Model o2) {
+                        return o2.getId() - o1.getId();
                     }
                 };
             }
         }
 
-        return res;
-    }
-
-    private static Comparator<Stage> compareStageBy(boolean title, final boolean asc) {
-        Comparator<Stage> res = BY_STAGE_DEFAULT;
-        if (title) {
-            res = new Comparator<Stage>() {
-                @Override
-                public int compare(Stage o1, Stage o2) {
-                    if (asc) {
-                        return o1.getTitle().compareTo(o2.getTitle());
-                    } else {
-                        return o2.getTitle().compareTo(o1.getTitle());
-                    }
-                }
-            };
-        } else {
-            if (asc) {
-                res = new Comparator<Stage>() {
-                    @Override
-                    public int compare(Stage o1, Stage o2) {
-                        return o1.getId() - o2.getId();
-                    }
-                };
-            }
-        }
-        return res;
+        return (Comparator<T>) res;
     }
 }
